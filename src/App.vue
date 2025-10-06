fa771008409238f413a4b92e9ec467f9b6181fe0
Paul van Genuchten <genuchten@yahoo.com>
2025-10-06 13:09:25 +0200

    move to browser supported libraries

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div class="container py-4">
-    <h1 class="mb-4">CSVW Editor</h1>
-    <CSVUploader @csv-loaded="onCsvLoaded" />
-    <CSVWClientParser
-      v-if="csvText"
-      :csvRows="csvRows"
-      :csvHeaders="csvHeaders"
-      :csvText="csvText"
-    />
-  </div>
+<div class="container py-4">
+<h1 class="mb-4">CSVW Browser Editor</h1>
+<CSVUploader @csv-loaded="onCsvLoaded" />
+<CSVWClientParser v-if="csvText" :csvRows="csvRows" :csvHeaders="csvHeaders" :csvText="csvText" />
+</div>
 </template>
 
 <script>
 import CSVUploader from './components/CSVUploader.vue'
 import CSVWClientParser from './components/CSVWClientParser.vue'
-
 export default {
-  components: { CSVUploader, CSVWClientParser },
-  data() {
-    return { csvRows: [], csvHeaders: [], csvText: '' }
-  },
-  methods: {
-    onCsvLoaded({ rows, headers, text }) {
-      this.csvRows = rows
-      this.csvHeaders = headers
-      this.csvText = text
-    }
-  }
+components: { CSVUploader, CSVWClientParser },
+data() { return { csvRows: [], csvHeaders: [], csvText: '' } },
+methods: {
+onCsvLoaded({ rows, headers, text }) {
+this.csvRows = rows
+this.csvHeaders = headers
+this.csvText = text
+}
+}
 }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
-      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      events: 'rollup-plugin-node-polyfills/polyfills/events',
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6'
-    }
-  }
+  base: './'
 })
